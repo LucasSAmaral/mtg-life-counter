4c7b4145a2a7743d76618315842a80ebff60212f
Lucas <lucassantosamaral@hotmail.com>
2020-08-09 14:34:53 -0300

    create state to control deck color

--- src/component/LifeCounter/LifeCounter.tsx
+++ src/component/LifeCounter/LifeCounter.tsx
@@ -1,13 +1,55 @@
 import React, { useState } from "react";
 import InputText from "../InputText/InputText";
 import CounterButton from "../CounterButton/CounterButton";
+import redIcon from "../../assets/icons/red.png";
+import blackIcon from "../../assets/icons/black.png";
+import blueIcon from "../../assets/icons/blue.png";
+import whiteIcon from "../../assets/icons/white.png";
+import greenIcon from "../../assets/icons/green.png";
 
 const LifeCounter: React.FC = () => {
   const [counterValue, setConterValue] = useState(20);
   const [valueAnimation, setValueAnimation] = useState("");
   const [isColorOptionsOpened, setIsColorOptionsOpened] = useState(false);
+  const [deckColor, setDeckColor] = useState("");
+
+  const manaDecks = [
+    {
+      colorIcon: redIcon,
+      color: "RED",
+    },
+    {
+      colorIcon: blueIcon,
+      color: "BLUE",
+    },
+    {
+      colorIcon: blackIcon,
+      color: "BLACK",
+    },
+    {
+      colorIcon: whiteIcon,
+      color: "WHITE",
+    },
+    {
+      colorIcon: greenIcon,
+      color: "GREEN",
+    },
+  ];
+
+  type colorsObjectType = {
+    [key: string]: string;
+  };
+
+  const colorsObject: colorsObjectType = {
+    RED: "--red-deck",
+    BLUE: "--blue-deck",
+    BLACK: "--black-deck",
+    WHITE: "--white-deck",
+    GREEN: "--green-deck",
+  };
+
   return (
-    <div className="mtg-life-counter-container">
+    <div className={`mtg-life-counter-container ${colorsObject[deckColor]}`}>
       <InputText />
       <div className="mtg-life-counter">
         <CounterButton
@@ -35,9 +77,13 @@ const LifeCounter: React.FC = () => {
             Change deck color
           </p>
           <ul className="mtg-deck-color-options">
-            <li>Primeira Opção</li>
-            <li>Segunda Opção</li>
-            <li>Terceira Opção</li>
+            {manaDecks.map((manaDeck) => (
+              <li key={manaDeck.color}>
+                <button onClick={() => setDeckColor(manaDeck.color)}>
+                  <img src={manaDeck.colorIcon} alt={manaDeck.color} />
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
