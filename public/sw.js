75d406c1691b86b77d37c36f0cd35140377c5940
Lucas <lucassantosamaral@hotmail.com>
2020-08-10 03:26:52 -0300

    update sw

--- public/sw.js
+++ public/sw.js
@@ -5,6 +5,7 @@ var urlsToCache = [
   "/mtg-life-counter/static/js/2.1b30836e.chunk.js",
   "/mtg-life-counter/static/js/main.4f529a54.chunk.js",
   "/mtg-life-counter/static/css/main.4fba246b.chunk.css",
+  "/mtg-life-counter/static/css/main.4fba246b.chunk.css?family=MTG",
   "/mtg-life-counter/static/js/runtime-main.45f50031.js",
   "/mtg-life-counter/favicon.ico",
 ];
