d5a64fb21486c7ffc6c1dfdb69bfad76440b3183
Lucas <lucassantosamaral@hotmail.com>
2020-08-19 22:17:20 -0300

    create types to some Objects

--- src/component/DeckColor.component.tsx
+++ src/component/DeckColor.component.tsx
@@ -4,16 +4,20 @@ import blackIcon from "../assets/icons/black.png";
 import blueIcon from "../assets/icons/blue.png";
 import whiteIcon from "../assets/icons/white.png";
 import greenIcon from "../assets/icons/green.png";
+import { Colors } from "./LifeCounter";
 
 type DeckColorComponentProps = {
-  setDeckColor: (color: string) => void;
+  setDeckColor: (color: Colors) => void;
 };
 
 const DeckColorComponent: React.FC<DeckColorComponentProps> = ({
   setDeckColor,
 }) => {
   const [isColorOptionsOpened, setIsColorOptionsOpened] = useState(false);
-  const manaDecks = [
+
+  type ManaDecks = { colorIcon: string; color: Colors }[];
+
+  const manaDecks: ManaDecks = [
     {
       colorIcon: redIcon,
       color: "RED",
--- src/component/LifeCounter.tsx
+++ src/component/LifeCounter.tsx
@@ -3,16 +3,19 @@ import InputText from "./InputText";
 import CounterButton from "./CounterButton";
 import DeckColorComponent from "./DeckColor.component";
 
+export type Colors = "RED" | "BLUE" | "BLACK" | "WHITE" | "GREEN" | "NONE";
+
 const LifeCounter: React.FC = () => {
   const [counterValue, setConterValue] = useState(20);
   const [valueAnimation, setValueAnimation] = useState("");
-  const [deckColor, setDeckColor] = useState("");
+  const [deckColor, setDeckColor] = useState<Colors>("NONE");
 
-  type colorsObjectType = {
-    [key: string]: string;
+  type ColorsObjectType = {
+    [Key in Colors]: string;
   };
 
-  const colorsObject: colorsObjectType = {
+  const colorsObject: ColorsObjectType = {
+    NONE: "",
     RED: "--red-deck",
     BLUE: "--blue-deck",
     BLACK: "--black-deck",
